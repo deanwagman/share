b6c8d9635f3a2944e7c9d49d2649f5acbf3ebc09
Dean Wagman <dean@purplerockscissors.com>
2014-08-29 15:26:25 -0400

    finish t fb pin

--- share.js
+++ share.js
@@ -2,7 +2,6 @@ var SHARE = (function(){
 	// Private Variables
 
 	// Concats object into URL search parameter
-		// Todo: Make sure each property is a property that the Social Network Accepts
 	var makeParamStr = function (obj) {
 		var paramStr = "";
 		for (var property in obj) {
@@ -13,24 +12,41 @@ var SHARE = (function(){
 		return paramStr;
 	};
 
+	var openInNewWindow = function (url, title, w, h) {
+		var dualSreenLeft, dualScreenTop, width, height, left, top, newWindow;
+
+        // Fixes dual-screen position                         Most browsers      Firefox
+        dualScreenLeft = window.screenLeft !== undefined ? window.screenLeft : screen.left;
+        dualScreenTop = window.screenTop !== undefined ? window.screenTop : screen.top;
+
+        width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
+        height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+
+        left = ((width / 2) - (w / 2)) + dualScreenLeft;
+        top = ((height / 3) - (h / 3)) + dualScreenTop;
+
+        newWindow = window.open(url, title, 'scrollbars=yes, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+
+        // Puts focus on the newWindow
+        if (window.focus) {
+            newWindow.focus();
+        }
+    };
+
 // Public shtuff
 	return{
 		twitter: function (options) {
 			var url = "https://twitter.com/share?" + makeParamStr(options);
-			window.location = url;
+			openInNewWindow(url, "twitter", 580, 250);
 		},
 		facebook: function (href) {
 			var url = "https://facebook.com/sharer/sharer.php?u=" + href;
-			window.location = url;
+			openInNewWindow(url, "facebook", 580, 368);
 		},
 		pinterest: function (options) {
 			var url = "https://www.pinterest.com/pin/create/button/?" + makeParamStr(options);
-			window.location = url;
+			openInNewWindow(url, "pinterest", 750, 320);
 		}
-	}	
+	};
 
 })();
-
-// setTimeout(function () {
-
-// }, 3000);
